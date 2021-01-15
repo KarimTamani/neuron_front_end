098733243a59165381e5da5f104413e1cf194a5a
KarimTamani <gk_tamani@esi.dz>
2021-01-15 19:36:27 +0100

    Calendar done!

--- src/app/components/dashboard/content/waiting-room/calendar/calendar.component.ts
+++ src/app/components/dashboard/content/waiting-room/calendar/calendar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-calendar',
@@ -7,9 +10,131 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  public monthes: string[] = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "October",
+    "Novembre",
+    "Décembre"
+  ];
 
-  ngOnInit(): void {
+  public days: string[] = [
+    "Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"
+  ];
+
+  public currentDate: Date;
+
+  public currentMonth: number;
+  public currentYear: number;
+  public currentDay: number;
+
+  public day: number;
+  public month: number;
+  public year: number;
+
+  public loaded: boolean = false;
+  //public schedule: Schedule;
+  ngOnInit(): void { }
+  constructor(private apollo: Apollo) {
+    /*this.dateService.getCurrentDate().subscribe((date) => {
+    })*/
+    this.apollo.query({
+      query: gql`
+        { 
+          getCurrentDate
+        }`
+    }).pipe(map(value => (<any>value.data).getCurrentDate)).subscribe(data => {
+
+      this.currentDate = new Date(data);
+
+      this.currentMonth = this.currentDate.getMonth();
+      this.currentYear = this.currentDate.getFullYear();
+      this.currentDay = this.currentDate.getDate();
+
+      this.day = this.currentDay;
+      this.month = this.currentMonth;
+      this.year = this.currentYear;
+
+      this.loaded = true;
+    })
+  }
+
+  nextMonth() {
+
+    this.day = 1;
+    if (this.currentMonth != 11)
+      this.currentMonth++;
+    else {
+      this.currentYear++;
+      this.currentMonth = 0;
+    }
+    if (this.currentYear == this.year && this.currentMonth == this.month) {
+      this.day = this.currentDay;
+    }
+    this.currentDate = new Date(this.currentMonth + 1 + " " + this.day + " " + this.currentYear);
+  }
+  previousMonth() {
+    this.day = 1;
+    if (this.currentMonth != 0)
+      this.currentMonth--;
+    else {
+      this.currentYear--;
+      this.currentMonth = 11;
+    }
+    if (this.currentYear == this.year && this.currentMonth == this.month) {
+      this.day = this.currentDay;
+    }
+    this.currentDate = new Date(this.currentMonth + 1 + " " + this.day + " " + this.currentYear);
+  }
+  private getStartOfMonth(date: Date) {
+    let time = date.getTime();
+    time = time - (date.getDate() - 1) * 24 * 3600 * 1000;
+    let startDate = new Date(time);
+    return startDate;
+  }
+
+  public getMonthDays(date: Date) {
+    let startDay = this.getStartOfMonth(date).getDay();
+    let days: string[] = [];
+    for (let index = startDay; index < this.days.length; index++) {
+      days.push(this.days[index]);
+    }
+    for (let index = 0; index < startDay; index++) {
+      days.push(this.days[index]);
+    }
+    return days;
+  }
+
+  public buildDays() {
+    let days = [];
+    let end = 31;
+    if ((this.currentMonth % 2) == 1) {
+      end = 30;
+    }
+    if (this.currentMonth == 1) {
+      if ((this.currentYear - 2020) % 4 == 0)
+        end = 29;
+      else
+        end = 28;
+    }
+    for (let index = 1; index <= end; index++)
+      days.push(index);
+    return days;
+  }
+  getWeek(week: number) {
+    let days = this.buildDays();
+    return days.slice(week * 7, (week * 7) + 7);
+  }
+
+  selectDay(d) {
+    this.day = d;
   }
 
 }
