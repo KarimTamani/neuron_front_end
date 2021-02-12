fae28611ffd72e899c0596094914cd92a05be966
KarimTamani <gk_tamani@esi.dz>
2021-02-12 11:35:36 +0100

    Search header design done

--- src/app/components/global/search-header/search-header.component.ts
+++ src/app/components/global/search-header/search-header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Wilaya } from 'src/app/classes/Wilaya';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-header',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-header.component.css']
 })
 export class SearchHeaderComponent implements OnInit {
-
-  constructor() { }
+  public wilayas : Wilaya[] = [] ;  
+  public selectedWilaya : Wilaya ; 
+  public selectedWilayaId : number ; 
+  constructor(private apollo : Apollo) {}
 
   ngOnInit(): void {
+  
+    this.apollo.query({
+      query : gql`
+        { 
+          getAllWilayas { 
+            id name communes { 
+              id name
+            }
+          }
+        }
+      `
+    }).pipe(map(value =>(<any>value.data).getAllWilayas)).subscribe((data) => { 
+      this.wilayas = data ;  
+    })
+  
   }
-
+  
+  public wilayaSelected() {
+    // filter by if to find the selected wilaya 
+    this.selectedWilaya = this.wilayas.find(wilaya => wilaya.id ==  this.selectedWilayaId);
+  }
 }
