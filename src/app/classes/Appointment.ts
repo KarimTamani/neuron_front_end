084e74126bb0a4a346cdda73e0df76818f3d9f3e
KarimTamani <gk_tamani@esi.dz>
2021-03-08 20:26:27 +0100

    Comming Appointments done

--- src/app/classes/Appointment.ts
+++ src/app/classes/Appointment.ts
@@ -1,7 +1,10 @@
+import { Visit } from "./Visit";
+
 export class Appointment { 
     public id : number ; 
     public date : string ; 
     public time : string ; 
+    public visit : Visit ; 
     public createdAt : string ; 
     public updatedAt : string ; 
 }
--- src/app/components/dashboard/content/analytics/comming-appointments/comming-appointments.component.ts
+++ src/app/components/dashboard/content/analytics/comming-appointments/comming-appointments.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
+import { Appointment } from 'src/app/classes/Appointment';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-comming-appointments',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommingAppointmentsComponent implements OnInit {
 
-  constructor() { }
+  public appointments : Appointment[] = []
+  constructor(private apollo : Apollo , private dataService : DataService) { }
 
   ngOnInit(): void {
+    
+    this.apollo.query({
+      query : gql`
+        {
+          getCurrentDate
+        }
+      `
+    }).pipe(map(value => (<any>value.data).getCurrentDate)).subscribe((data) => { 
+      const currentDate = this.dataService.castDateYMD(data)  ;
+
+      this.apollo.query({
+        query : gql`
+          {
+            searchAppointments(startDate : "${currentDate}") { 
+              rows {
+                date 
+                time 
+                visit {
+                  medicalFile {
+                    id 
+                    name 
+                    lastname 
+                    gender 
+                    birthday
+                    phone 
+                    email
+                  }
+                }
+              }
+            }
+          }`
+      }).pipe(map( value => (<any>value.data).searchAppointments)).subscribe((data) => { 
+        this.appointments = data ; 
+        console.log(this.appointments) ; 
+      })
+    })
   }
 
 }
