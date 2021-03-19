daa0d06922e80e3666696c760dd64b6f7ed9b0b8
KarimTamani <gk_tamani@esi.dz>
2021-03-19 17:31:05 +0100

    Documents Manager done

--- src/app/components/dashboard/content/documents-and-diagnosis/documents-manager/documents-manager.component.ts
+++ src/app/components/dashboard/content/documents-and-diagnosis/documents-manager/documents-manager.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
+import { Document } from 'src/app/classes/Document';
 
 @Component({
   selector: 'app-documents-manager',
@@ -6,10 +10,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./documents-manager.component.css']
 })
 export class DocumentsManagerComponent implements OnInit {
+  public offset: number = 0;
+  public limit: number = 10;
+  public lastSearch: any = {};
+  public count: number = 0;
+  public documents: Document[] = [];
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+    this.loadDocuments(null, null, null);
+  }
+
+
+  private loadDocuments(searchQuery = null, startDate = null, endDate = null) {
+    this.apollo.query({
+      query: gql`
+        query SEARCH_DOCUMNETS($searchQuery : String, $startDate : String , $endDate : String , $offset : Int , $limit : Int) { 
+          searchDocuments(
+            searchQuery : $searchQuery , 
+            startDate : $startDate , 
+            endDate : $endDate , 
+            offset : $offset , 
+            limit : $limit 
+          ) { 
+            rows {
+              path
+              createdAt
+              updatedAt
+              description
+              name
+              visit {
+                medicalFile { id name gender lastname birthday phone email}
+              }
+            }
+            count        
+          }
+        }`
+      , variables: {
+        searchQuery: searchQuery,
+        startDate: startDate,
+        endDate: endDate,
+        offset: this.offset,
+        limit: this.limit
+      }
+    }).pipe(map(value => (<any>value.data).searchDocuments)).subscribe((data) => {
+      this.documents = data.rows;
+      console.log(this.documents);
+      this.count = data.count;
+    })
+  }
+
+
+  search($event) {
+    this.offset = 0;
+    this.lastSearch = $event;
+    this.loadDocuments(
+      $event.searchQuery,
+      $event.startDate,
+      $event.endDate
+    );
+  }
+  selectPage($event) {
+    this.offset = $event;
+    this.loadDocuments(
+      this.lastSearch.searchQuery,
+      this.lastSearch.startDate,
+      this.lastSearch.ednDate
+    )
   }
 
 }
