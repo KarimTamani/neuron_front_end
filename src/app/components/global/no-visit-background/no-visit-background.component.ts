caa54d733c37031e4b058c25ac71443a59f275ed
KarimTamani <gk_tamani@esi.dz>
2021-03-28 16:16:02 +0100

    NoVisitBackground done

--- src/app/components/global/no-visit-background/no-visit-background.component.ts
+++ src/app/components/global/no-visit-background/no-visit-background.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-no-visit-background',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./no-visit-background.component.css']
 })
 export class NoVisitBackgroundComponent implements OnInit {
-
+  @Input() message : string ; 
   constructor() { }
 
   ngOnInit(): void {
